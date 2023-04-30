83043905896892cb3859500e0b4e71ce6aa9001b
Sheetal <Sheetalwr23@gmail.com>
2023-04-30 12:25:06 +0530

    deleting the appointments

---
+++ apponment booking/app.js
@@ -0,0 +1,81 @@
+"use strict";
+function saveToLocalStorage(event) {
+  let submitName = event.target.username.value;
+  let email = event.target.email.value;
+  event.preventDefault();
+  let obj = {};
+  obj.Name = submitName;
+  obj.Email = email;
+  // let obj_serialized = JSON.stringify(obj);
+  // localStorage.setItem(obj.Email, obj_serialized);
+  axios
+    .post(
+      "https://crudcrud.com/api/5d2ae201480d486eac7c30eb4fb99149/Appointmentdata",
+      obj
+    )
+    .then((response) => {
+      showUserOnScreen(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  // showUserOnScreen(obj);
+}
+window.addEventListener("DOMContentLoaded", () => {
+  axios
+    .get(
+      "https://crudcrud.com/api/5d2ae201480d486eac7c30eb4fb99149/Appointmentdata"
+    )
+    .then((res) => {
+      // console.log(res);
+      for (var i = 0; i < res.data.length; i++) {
+        showUserOnScreen(res.data[i]);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+function showUserOnScreen(obj) {
+  const parentElement = document.getElementById("my-form");
+  const childElement = document.createElement("li");
+  childElement.textContent = `${obj.Name}-${obj.Email}  `;
+  const button = document.createElement("input");
+  button.type = "button";
+  button.value = "Delete";
+  button.onclick = () => {
+    // localStorage.removeItem(obj.Email);
+    axios
+      .delete(
+        `https://crudcrud.com/api/5d2ae201480d486eac7c30eb4fb99149/Appointmentdata/${obj._id}`
+      )
+      .then((res) => {
+        // console.log(res);
+      })
+      .catch((err) => console.log(err));
+    parentElement.removeChild(childElement);
+  };
+  const EditButton = document.createElement("input");
+  EditButton.type = "button";
+  EditButton.value = "Edit";
+  EditButton.onclick = () => {
+    // localStorage.removeItem(obj.Email);
+    axios
+      .delete(
+        `https://crudcrud.com/api/5d2ae201480d486eac7c30eb4fb99149/Appointmentdata/${obj._id}`
+      )
+
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    parentElement.removeChild(childElement);
+    document.getElementById("id-name").value = obj.Name;
+    document.getElementById("id-email").value = obj.Email;
+  };
+  childElement.appendChild(button);
+  childElement.appendChild(EditButton);
+  parentElement.appendChild(childElement);
+}
